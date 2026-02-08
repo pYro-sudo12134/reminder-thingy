5e0cf326acbd6787eab38d9923e6dc6f7ab050fb
pYro-sudo12134 <firefox23769801@gmail.com>
2026-02-09 01:44:24 +0300

    nfs

--- nfs-export.sh
+++ nfs-export.sh
@@ -1,2 +1,16 @@
+#!/bin/bash
 mkdir -p nfs-config
-echo "/exports 10.10.0.0/24(rw,sync,no_subtree_check,root_squash,fsid=0)" > nfs-config/exports
+
+cat > nfs-config/exports << EOF
+/exports 10.10.0.0/24(rw,sync,fsid=0,no_subtree_check,no_root_squash,insecure,crossmnt)
+EOF
+
+mkdir -p /nfs-storage
+mkdir -p /nfs-storage/nlp_models
+mkdir -p /nfs-storage/nlp_data
+mkdir -p /nfs-storage/localstack_data
+mkdir -p /nfs-storage/postgres_backup
+mkdir -p /nfs-storage/app_static
+
+chown -R nobody:nogroup /nfs-storage
+chmod -R 777 /nfs-storage
